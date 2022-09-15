f82e6231fe97a5891ba56020c4074e3a816e892c
zJuarez <mji2000.741@gmail.com>
2022-09-15 13:49:20 -0500

    tic tac toe

---
+++ src/Game.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import Board from './Board';
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
+function Game() {
+    const [history, setHistory] = useState(Array(1).fill({
+        squares: Array(9).fill(null)
+    }));
+    const [stepNumber, setStepNumber] = useState(0);
+    const [xIsNext, setXIsNext] = useState(true);
+
+
+    const handleClick = (i) => {
+        const historyX = history.slice(0, stepNumber + 1);
+        const current = historyX[historyX.length - 1];
+        const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = xIsNext ? "X" : "O";
+        setHistory(historyX.concat([{ squares: squares }]))
+        setStepNumber(historyX.length)
+        setXIsNext(!xIsNext)
+    }
+
+    const restart = () => {
+        setHistory(Array(1).fill({
+            squares: Array(9).fill(null)
+        }));
+        setStepNumber(0);
+        setXIsNext(true);
+    }
+    const jumpTo = (step) => {
+        setStepNumber(step);
+        setXIsNext((step % 2) === 0);
+    }
+
+    const current = history[stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+        const desc = move ?
+            'Go to move #' + move :
+            'Go to game start';
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>{desc}</button>
+            </li>
+        );
+    });
+
+
+    let status;
+    if (winner) {
+        status = "Winner: " + winner;
+    } else {
+        status = "Next player: " + (xIsNext ? "X" : "O");
+    }
+
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board
+                    squares={current.squares}
+                    onClick={i => handleClick(i)}
+                />
+            </div>
+            <div className="game-info">
+                <h4>{status}</h4>
+                <ol>{moves}</ol>
+                <button onClick={() => restart()}> Restart</button>
+            </div>
+        </div>
+    );
+
+}
+
+export default Game;
